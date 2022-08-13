0d2707c09bccea09e3078ea32ed860c1b448d507
M.a_k <m.a_k@mail.ru>
2022-08-13 22:59:32 +0600

    with comments

--- trees/delete.go
+++ trees/delete.go
@@ -16,46 +16,47 @@ func main() {
 	a.Insert(4)
 	fmt.Println("before delition:")
 	a.PrintInOrder()
-	a.delete(5)
+	a.delete(4)
 	fmt.Println("deletion ")
 	fmt.Println("after delition:")
 	a.PrintInOrder()
 }
 
 func (n *node) delete(value int) *node {
-	if n == nil {
+	if n == nil { // if node is empty
+		fmt.Println()
+		return nil // do nothing
+	}
+	if value > n.val { // if the value we want delete is larger than current node's value
+		n.right = n.right.delete(value) // move to right branch
+		return n
+	}
+	if value < n.val { // if the value we want to delete is smaller than current node's value
+		n.left = n.left.delete(value) // move to the lift branch
+		return n
+	}
+	if n.right == nil && n.left == nil { // if left and right branch are empty
+		n = nil // just delete its
 		return nil
 	}
-	if value > n.val {
-		n.right = n.right.delete(value)
+	if n.left == nil { // if left node is empty
+		n = n.right // move to the right node
 		return n
 	}
-	if value < n.val {
-		n.left = n.left.delete(value)
+	if n.right == nil { // if right node is empty
+		n = n.left // move to the left node
 		return n
 	}
-	if n.right == nil && n.left == nil {
-		n = nil
-		return nil
-	}
-	if n.left == nil {
-		n = n.right
-		return n
-	}
-	if n.right == nil {
-		n = n.left
-		return n
-	}
-	smallest := n.right
-	for {
-		if smallest != nil && smallest.left != nil {
-			smallest = smallest.left
+	smallest := n.right // assign right node to variable to find it in right subtree
+	for {               // infinite loop
+		if smallest != nil && smallest.left != nil { // if right node is not empty and its left node is not empty
+			smallest = smallest.left // change right node's to its left node value
 		} else {
-			break
+			break // in any other variant stop the loop
 		}
 	}
-	n.val = smallest.val
-	n.right = n.right.delete(n.val)
+	n.val = smallest.val            // node's value now equal = to the smallest val in the right subtree
+	n.right = n.right.delete(n.val) // and we trasfer its right node to the delete function
 	return n
 }
 
